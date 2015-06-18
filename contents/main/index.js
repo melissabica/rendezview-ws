7376bf7ad97242a9b81e4c5ff827bcc5dacabcec
melissab147 <melissa.j.bica@gmail.com>
2015-06-18 10:03:03 +0900

    working example with sidebar and one cube that rotates/pans/zooms

--- contents/main/index.js
+++ contents/main/index.js
@@ -2,18 +2,16 @@ var main = {
 
     load: function() {
 
+        //sidebar
         $.get("/sophy/main/ui.jade", function(template) {
             var htmlSidebar = jade.render(template)
             $("#ui").html(htmlSidebar)
         })
 
-        $.get("/sophy/main/list.jade", function(template) {
-            var htmlMain = jade.render(template)
-        })
-
-        $.get("/sophy/main/test.html", function(html) {
-            console.log(html)
-            $("#list").html(html)            
+        //three.js visualization
+        $.get("/sophy/main/test2.html", function(html) {
+            // console.log(html)
+            $("#list").html(html)
         })
 
     }
