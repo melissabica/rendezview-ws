de06ad4c7cf44624adefbeda71efff340f791b19
melissab147 <melissa.j.bica@gmail.com>
2015-06-23 16:33:25 +0900

    static tag cloud in corner

--- contents/main/index.js
+++ contents/main/index.js
@@ -8,6 +8,12 @@ var main = {
             $("#ui").html(htmlSidebar)
         })
 
+        //upper toolbar
+        $.get("/sophy/main/toolbar.html", function(html) {
+            // console.log(html)
+            $("#upper").html(html)
+        })
+
         //three.js visualization
         $.get("/sophy/main/visualization.html", function(html) {
             // console.log(html)
