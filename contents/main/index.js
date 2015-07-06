45e91aef8c88e8b90569a6981e4b86a5b9f3428a
melissab147 <melissa.j.bica@gmail.com>
2015-07-06 14:35:24 +0900

    converted ui and toolbar to jade templates

--- contents/main/index.js
+++ contents/main/index.js
@@ -6,33 +6,30 @@ var main = {
             options.async = true;
         });        
 
-        //sidebar
-        // $.get("/sophy/main/ui.jade", function(template) {
-        //     var htmlSidebar = jade.render(template);
-        //     $("#ui").html(htmlSidebar);
+        // Sidebar
+        $.get("/sophy/main/ui.jade", function(template) {
+            var htmlSidebar = jade.render(template);
+            $("#ui").html(htmlSidebar);
+        })
+        // $.get("/sophy/main/ui.html", function(html) {
+        //     $("#ui").html(html);
         // })
-        $.get("/sophy/main/ui.html", function(html) {
-            $("#ui").html(html);
-        })
 
-        //upper toolbar
-        $.get("/sophy/main/toolbar.html", function(html) {
-            // console.log(html);
-            $("#upper").html(html);
+        // Upper toolbar
+        $.get("/sophy/main/toolbar.jade", function(template) {
+            var htmlToolbar = jade.render(template);
+            $("#upper").html(htmlToolbar);
         })
+        // $.get("/sophy/main/toolbar.html", function(html) {
+        //     // console.log(html);
+        //     $("#upper").html(html);
+        // })
 
-        //three.js visualization
+        // three.js visualization
         $.get("/sophy/main/visualization.html", function(html) {
             // console.log(html);
             $("#visualization").html(html);
         })
-
-        // //three.js visualization
-        // $.get("/sophy/main/visualization.jade", function(template) {
-        //     var html = jade.render(template);
-        //     $("#list").html(html);
-        // })
-
 
     }
 
