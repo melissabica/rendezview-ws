059e0a5b7c1a16898c1e68e44c2e2dabb73d7d97
melissab147 <melissa.j.bica@gmail.com>
2015-07-09 15:09:13 +0900

    changed name/url from sophy to rendezview-ws

--- contents/main/index.js
+++ contents/main/index.js
@@ -7,26 +7,26 @@ var main = {
         });        
 
         // Sidebar
-        $.get("/sophy/main/ui.jade", function(template) {
+        $.get("/rendezview/main/ui.jade", function(template) {
             var htmlSidebar = jade.render(template);
             $("#ui").html(htmlSidebar);
         })
-        // $.get("/sophy/main/ui.html", function(html) {
+        // $.get("/rendezview/main/ui.html", function(html) {
         //     $("#ui").html(html);
         // })
 
         // Upper toolbar
-        $.get("/sophy/main/toolbar.jade", function(template) {
+        $.get("/rendezview/main/toolbar.jade", function(template) {
             var htmlToolbar = jade.render(template);
             $("#upper").html(htmlToolbar);
         })
-        // $.get("/sophy/main/toolbar.html", function(html) {
+        // $.get("/rendezview/main/toolbar.html", function(html) {
         //     // console.log(html);
         //     $("#upper").html(html);
         // })
 
         // three.js visualization
-        $.get("/sophy/main/visualization.html", function(html) {
+        $.get("/rendezview/main/visualization.html", function(html) {
             // console.log(html);
             $("#visualization").html(html);
         })
